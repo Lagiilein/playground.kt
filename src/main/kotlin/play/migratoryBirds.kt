cbcc540c8d3197978d26de339d4e8dbe6a28d4b5
ByLagii <ByLagii@gmail.com>
2021-07-17 14:58:17 +0200

    add migratory birds

---
+++ src/main/kotlin/play/migratoryBirds.kt
@@ -0,0 +1,30 @@
+package play
+
+/**
+ * [https://www.hackerrank.com/challenges/migratory-birds/problem]
+ */
+fun migratoryBirds(arr: Array<Int>): Int {
+
+    val frequencies = mutableMapOf<Int, Int>()
+
+    arr.forEach {
+        frequencies[it] = (frequencies[it] ?: 0) + 1
+    }
+
+    var biggestId = arr[0]
+    var currentBiggest = 0
+
+    frequencies.onEach {
+        if (it.value > currentBiggest) {
+            biggestId = it.key
+            currentBiggest = it.value
+        } else if (it.value == currentBiggest) {
+            if (it.key < biggestId) {
+                biggestId = it.key
+                currentBiggest = it.value
+            }
+        }
+    }
+
+    return biggestId
+}

11fb9f2d8c62efdadc81c7bd888a07651a56b7e2
ByLagii <ByLagii@gmail.com>
2021-07-19 18:13:52 +0200

    add quick sort and insertion sort implementations;

---
+++ src/main/kotlin/play/sort/Quicksort.kt
@@ -0,0 +1,312 @@
+package play.sort
+
+private fun <T> MutableList<T>.swap(firstIndex: Int, secondIndex: Int) {
+    val e = this[firstIndex]
+    this[firstIndex] = this[secondIndex]
+    this[secondIndex] = e
+}
+
+private fun <T : Comparable<T>> MutableList<T>.quickSortPartition(lo: Int, hi: Int): Int {
+    val pivot = this[(lo + hi) / 2]
+
+    var i = lo - 1
+    var j = hi + 1
+
+    while (true) {
+        do {
+            i++
+        } while (this[i] < pivot)
+
+        do {
+            j--
+        } while (this[j] > pivot)
+
+        if (i >= j) {
+            return j
+        }
+
+        this.swap(j, i)
+    }
+}
+
+fun <T : Comparable<T>> MutableList<T>.quickSort(lo: Int = 0, hi: Int = size - 1) {
+    if (lo < hi) {
+        val p = quickSortPartition(lo, hi)
+        quickSort(lo, p)
+        quickSort(p + 1, hi)
+    }
+}
+
+/**
+ * uses QuickSort, but if the number of elements goes under a certain threshold, it switches to InsertionSort.
+ */
+fun <T : Comparable<T>> MutableList<T>.quickInsertionSort(lo: Int = 0, hi: Int = size - 1, threshold: Int = 6) {
+    if (lo < hi) {
+        if (hi - lo <= threshold) {
+            this.insertionSort(lo, hi)
+        } else {
+            val p = quickSortPartition(lo, hi)
+            quickInsertionSort(lo, p)
+            quickInsertionSort(p + 1, hi)
+        }
+    }
+}
+
+internal fun <T : Comparable<T>> MutableList<T>.quickSort1() {
+    fun swap(first: Int, second: Int) {
+        val e = this[first]
+        this[first] = this[second]
+        this[second] = e
+    }
+
+    fun partition(lo: Int, hi: Int): Int {
+        val pivot = this[hi]
+        var i = lo
+
+        for (j in lo until hi) {
+            if (this[j] < pivot) {
+                swap(i, j)
+                i++
+            }
+        }
+        swap(i, hi)
+        return i
+    }
+
+    fun quickSort(lo: Int, hi: Int) {
+        if (lo < hi) {
+            val p = partition(lo, hi)
+            quickSort(lo, p - 1)
+            quickSort(p + 1, hi)
+        }
+    }
+
+    quickSort(this.indices.first, this.indices.last)
+}
+
+internal fun <T : Comparable<T>> MutableList<T>.quickSort2() {
+    fun swap(first: Int, second: Int) {
+        val e = this[first]
+        this[first] = this[second]
+        this[second] = e
+    }
+
+    fun partition(lo: Int, hi: Int): Int {
+
+        val mid = (lo + hi) / 2
+
+        if (this[mid] < this[lo]) {
+            swap(lo, mid)
+        } else if (this[hi] < this[lo]) {
+            swap(lo, hi)
+        } else if (this[mid] < this[hi]) {
+            swap(mid, hi)
+        }
+
+        val pivot = this[hi]
+
+        var i = lo
+
+        for (j in lo until hi) {
+            if (this[j] < pivot) {
+                swap(i, j)
+                i++
+            }
+        }
+        swap(i, hi)
+        return i
+    }
+
+    fun quickSort(lo: Int, hi: Int) {
+        if (lo < hi) {
+            val p = partition(lo, hi)
+            quickSort(lo, p - 1)
+            quickSort(p + 1, hi)
+        }
+    }
+
+    quickSort(this.indices.first, this.indices.last)
+}
+
+internal fun <T : Comparable<T>> MutableList<T>.quickSort3() {
+    fun swap(first: Int, second: Int) {
+        val e = this[first]
+        this[first] = this[second]
+        this[second] = e
+    }
+
+    fun partition(lo: Int, hi: Int): Int {
+
+        val pivot = this[(lo + hi) / 2]
+
+        var i = lo - 1
+        var j = hi + 1
+
+        while (true) {
+            do {
+                i++
+            } while (this[i] < pivot)
+
+            do {
+                j--
+            } while (this[j] > pivot)
+
+            if (i >= j) {
+                return j
+            }
+
+            swap(j, i)
+        }
+    }
+
+    fun quickSort(lo: Int, hi: Int) {
+        if (lo < hi) {
+            val p = partition(lo, hi)
+            quickSort(lo, p)
+            quickSort(p + 1, hi)
+        }
+    }
+
+    quickSort(this.indices.first, this.indices.last)
+}
+
+internal fun <T : Comparable<T>> MutableList<T>.quickSort4() {
+    fun swap(first: Int, second: Int) {
+        val e = this[first]
+        this[first] = this[second]
+        this[second] = e
+    }
+
+    fun partition(lo: Int, hi: Int): Int {
+
+        val mid = (lo + hi) / 2
+
+        if (this[mid] < this[lo]) {
+            swap(lo, mid)
+        } else if (this[hi] < this[lo]) {
+            swap(lo, hi)
+        } else if (this[mid] < this[hi]) {
+            swap(mid, hi)
+        }
+
+        val pivot = this[hi]
+
+        var i = lo - 1
+        var j = hi + 1
+
+        while (true) {
+            do {
+                i++
+            } while (this[i] < pivot)
+
+            do {
+                j--
+            } while (this[j] > pivot)
+
+            if (i >= j) {
+                return j
+            }
+
+            swap(j, i)
+        }
+    }
+
+    fun quickSort(lo: Int, hi: Int) {
+        if (lo < hi) {
+            val p = partition(lo, hi)
+            quickSort(lo, p)
+            quickSort(p + 1, hi)
+        }
+    }
+
+    quickSort(this.indices.first, this.indices.last)
+}
+
+internal fun <T : Comparable<T>> MutableList<T>.quickSort5() {
+    fun partition(lo: Int, hi: Int): Int {
+        val pivot = this[hi]
+
+        var i = lo
+        var j = hi - 1
+
+        while (i < j) {
+            while (i < hi && this[i] < pivot) {
+                i++
+            }
+
+            while (j > lo && this[j] >= pivot) {
+                j--
+            }
+
+            if (i < j) {
+                swap(i, j)
+            }
+        }
+
+        if (this[i] > pivot) {
+            swap(i, hi)
+        }
+
+        return i
+    }
+
+    fun quickSort(lo: Int, hi: Int) {
+        if (lo < hi) {
+            val p = partition(lo, hi)
+            quickSort(lo, p - 1)
+            quickSort(p + 1, hi)
+        }
+    }
+
+    quickSort(this.indices.first, this.indices.last)
+}
+
+internal fun <T : Comparable<T>> MutableList<T>.quickSort6() {
+    fun partition(lo: Int, hi: Int): Int {
+
+        val mid = (lo + hi) / 2
+
+        if (this[mid] < this[lo]) {
+            swap(lo, mid)
+        } else if (this[hi] < this[lo]) {
+            swap(lo, hi)
+        } else if (this[mid] < this[hi]) {
+            swap(mid, hi)
+        }
+
+        val pivot = this[hi]
+
+        var i = lo
+        var j = hi - 1
+
+        while (i < j) {
+            while (i < hi && this[i] < pivot) {
+                i++
+            }
+
+            while (j > lo && this[j] >= pivot) {
+                j--
+            }
+
+            if (i < j) {
+                swap(i, j)
+            }
+        }
+
+        if (this[i] > pivot) {
+            swap(i, hi)
+        }
+
+        return i
+    }
+
+    fun quickSort(lo: Int, hi: Int) {
+        if (lo < hi) {
+            val p = partition(lo, hi)
+            quickSort(lo, p - 1)
+            quickSort(p + 1, hi)
+        }
+    }
+
+    quickSort(this.indices.first, this.indices.last)
+}
---
+++ src/test/kotlin/play/sort/SortTest.kt
@@ -0,0 +1,140 @@
+package play.sort
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import kotlin.random.Random
+import kotlin.time.Duration
+import kotlin.time.ExperimentalTime
+import kotlin.time.measureTime
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class SortTest {
+    private val testLists: MutableList<MutableList<Int>> =
+        mutableListOf(
+            mutableListOf(
+                0, 532, 563, 63, 643, 643, 42, 432, 5, 21, -253, 325
+            )
+        )
+
+    private val testMethods: MutableMap<String, (it: MutableList<Long>) -> Unit> =
+        mutableMapOf()
+
+    @BeforeAll
+    @Suppress("unused")
+    fun before() {
+        testMethods[".sort()"] = {
+            it.sort()
+        }
+
+        testMethods["quicksort"] = {
+            it.quickSort()
+        }
+
+        testMethods["quicksort1"] = {
+            it.quickSort1()
+        }
+
+        testMethods["quicksort2"] = {
+            it.quickSort2()
+        }
+
+        testMethods["quicksort3"] = {
+            it.quickSort3()
+        }
+
+        testMethods["quicksort4"] = {
+            it.quickSort4()
+        }
+
+        testMethods["quicksort5"] = {
+            it.quickSort5()
+        }
+
+        testMethods["quicksort6"] = {
+            it.quickSort6()
+        }
+
+        testMethods["insertion sort"] = {
+            it.insertionSort()
+        }
+
+        testMethods["quick insertion sort (12)"] = {
+            it.quickInsertionSort(threshold = 12)
+        }
+
+        testMethods["quick insertion sort (6)"] = {
+            it.quickInsertionSort(threshold = 6)
+        }
+
+        testMethods["quick insertion sort (8)"] = {
+            it.quickInsertionSort(threshold = 8)
+        }
+
+        testMethods["quick insertion sort (4)"] = {
+            it.quickInsertionSort(threshold = 4)
+        }
+    }
+
+    @OptIn(ExperimentalTime::class)
+    @Test
+    fun `test sorting algorithms`() {
+        val times = mutableMapOf<String, MutableList<Duration>>()
+
+        fun testList(it: MutableList<Long>) {
+            for ((name, block) in testMethods) {
+                val list = it.toMutableList()
+
+                val time = measureTime {
+                    block(list)
+                }
+
+                val prevTimes = times[name] ?: mutableListOf()
+                prevTimes.add(time)
+                times[name] = prevTimes
+
+                Assertions.assertArrayEquals(
+                    list.toTypedArray(), it.toMutableList().sorted().toTypedArray(), """
+                        method: $name
+                        original: $it
+                        result: $list
+                    """.trimIndent()
+                )
+
+                println("$name - $time - ${list.size}")
+            }
+        }
+
+        testLists.forEach { testList ->
+            testList(testList.map { it.toLong() }.toMutableList())
+        }
+
+        for (i in 0..4) {
+            val newList = mutableListOf<Long>()
+
+            for (j in 0..(Random.nextInt(50000))) {
+                newList.add(Random.nextLong())
+            }
+
+            testList(newList)
+        }
+
+        for (i in 0..4) {
+            val newList = mutableListOf<Long>()
+
+            Random.nextBytes(Random.nextInt(50000)).forEach {
+                newList.add(it.toLong())
+            }
+
+            testList(newList)
+        }
+
+
+        for ((name, durations) in times) {
+            val sum = Duration.nanoseconds(durations.sumOf { it.inWholeNanoseconds })
+            val avg = Duration.nanoseconds(sum.inWholeNanoseconds / durations.size)
+            println("$name - avg: $avg - sum: $sum - $durations")
+        }
+    }
+}

d1861af4eccc09cd5d6348b3a1c2bd23797f4303
ByLagii <ByLagii@gmail.com>
2021-07-21 14:29:51 +0200

    add extra long factorials;

---
+++ src/test/kotlin/play/ExtraLongFactorialsTest.kt
@@ -0,0 +1,14 @@
+package play
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import java.math.BigInteger
+
+internal class ExtraLongFactorialsTest {
+
+    @Test
+    fun `test extra long factorials`() {
+        assertEquals(BigInteger("15511210043330985984000000"), extraLongFactorials(25))
+        assertEquals(BigInteger("265252859812191058636308480000000"), extraLongFactorials(30))
+    }
+}

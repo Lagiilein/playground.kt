07b6af6640fcac042f51a2d2fcc3e6d09a6fe548
ByLagii <ByLagii@gmail.com>
2021-08-15 14:54:06 +0200

    switch from measureNanoTime to measureTime;

--- src/test/kotlin/play/TwoSumTest.kt
+++ src/test/kotlin/play/TwoSumTest.kt
@@ -2,19 +2,21 @@ package play
 
 import org.junit.jupiter.api.Assertions.assertArrayEquals
 import org.junit.jupiter.api.Test
-import kotlin.system.measureNanoTime
+import kotlin.time.ExperimentalTime
+import kotlin.time.measureTime
 
 internal class TwoSumTest {
 
+    @OptIn(ExperimentalTime::class)
     @Test
     fun twoSum() {
-        val t1 = measureNanoTime {
+        val t1 = measureTime {
             assertArrayEquals(intArrayOf(0, 1), twoSum(intArrayOf(2, 7, 11, 15), 9))
             assertArrayEquals(intArrayOf(1, 2), twoSum(intArrayOf(3, 2, 4), 6))
             assertArrayEquals(intArrayOf(0, 1), twoSum(intArrayOf(3, 3), 6))
         }
 
-        val t2 = measureNanoTime {
+        val t2 = measureTime {
             assertArrayEquals(intArrayOf(0, 1), twoSumFast(intArrayOf(2, 7, 11, 15), 9))
             assertArrayEquals(intArrayOf(1, 2), twoSumFast(intArrayOf(3, 2, 4), 6))
             assertArrayEquals(intArrayOf(0, 1), twoSumFast(intArrayOf(3, 3), 6))

f261a94a784122992e50950b7b4544e9ecc3db98
ByLagii <ByLagii@gmail.com>
2021-09-05 09:18:11 +0200

    add drawing book

---
+++ src/test/kotlin/play/DrawingBookTest.kt
@@ -0,0 +1,18 @@
+package play
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class DrawingBookTest {
+
+    @Test
+    fun `test pageTurnsNeeded`() {
+
+        assertEquals(1, pageTurnsNeeded(5, 3))
+        assertEquals(1, pageTurnsNeeded(6, 2))
+        assertEquals(0, pageTurnsNeeded(5, 4))
+        assertEquals(0, pageTurnsNeeded(4, 4))
+        assertEquals(1, pageTurnsNeeded(6, 4))
+        assertEquals(1, pageTurnsNeeded(6, 5))
+    }
+}

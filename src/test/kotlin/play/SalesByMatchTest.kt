d0d4eb05a10da3788e45e609319401c0ac5d0dec
ByLagii <ByLagii@gmail.com>
2021-09-05 08:49:33 +0200

    add sales by match

---
+++ src/test/kotlin/play/SalesByMatchTest.kt
@@ -0,0 +1,15 @@
+package play
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class SalesByMatchTest {
+
+    @Test
+    fun `test sockMerchant`() {
+
+        assertEquals(2, sockMerchant(7, arrayOf(1, 2, 1, 2, 1, 3, 2)))
+        assertEquals(3, sockMerchant(9, arrayOf(10, 20, 20, 10, 10, 30, 50, 10, 20)))
+
+    }
+}

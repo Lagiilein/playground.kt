9634767d5aa30ad3ec922f5846b6bb54293494af
ByLagii <ByLagii@gmail.com>
2021-09-19 11:53:00 +0200

    optimize DivisibleSumPairs

--- core/src/main/kotlin/play/DivisibleSumPairs.kt
+++ core/src/main/kotlin/play/DivisibleSumPairs.kt
@@ -3,18 +3,32 @@ package play
 /**
  * [https://www.hackerrank.com/challenges/divisible-sum-pairs/problem]
  */
-fun divisibleSumPairs(ignored: Int, k: Int, ar: Array<Int>): Int {
+fun divisibleSumPairs(ignored: Int, k: Int, arr: Array<Int>): Int {
     var pairs = 0
 
-    for (i in ar.indices) {
-        for (j in (i + 1) until ar.size) {
-            if (i >= j) continue // shouldn't happen, but just in case
+    val buckets = List<MutableList<Int>>(k) {
+        mutableListOf()
+    }
 
-            val sum = ar[i] + ar[j]
-            if (sum % k == 0) {
-                pairs++
-            }
-        }
+    // fill buckets with input
+    for (item in arr) {
+        buckets[item % k] += item
+    }
+
+    // add number of pairs of bucket 0
+    val zeroBucketSize = buckets[0].size
+    pairs += zeroBucketSize * (zeroBucketSize - 1) / 2
+
+    // add number of pairs of other buckets
+    for (i in 1..(k / 2)) {
+        if (k % 2 == 0 && i == k / 2) continue
+        pairs += buckets[i].size * buckets[k - i].size
+    }
+
+    // add number of pairs of the half k bucket
+    if (k % 2 == 0) {
+        val halfKBucketSize = buckets[k / 2].size
+        pairs += halfKBucketSize * (halfKBucketSize - 1) / 2
     }
 
     return pairs
